0d76487237cbdb8b6a3ece3fb33f295622e5b7d2
matheus <matheussandi@hotmail.com>
2023-09-09 18:02:36 -0300

    :sparkles: feat: Adiciona funcionalidade de filtragem de consultas
    - Permite que clínicas e médicos visualizem consultas passadas, de hoje e futuras
    - Adiciona opções de filtro no painel de consultas para uma melhor organização
    - Aprimora a experiência do usuário ao facilitar o acesso às informações necessárias

--- web/src/app/clinic/[clinicId]/doctorSelected/patients/page.tsx
+++ web/src/app/clinic/[clinicId]/doctorSelected/patients/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-
 import { getPatients } from "../../../../../services/get-patients";
-
 import Pacientes from "../../../../../assets/doctor1.png";
 import { MdDateRange } from "react-icons/md";
 import { FiClock } from "react-icons/fi";
@@ -15,7 +14,7 @@ import dayjs from "dayjs";
 
 dayjs.locale(ptBr);
 
-interface PatientesProps {
+interface PatientsProps {
   id: number;
   nome: string;
   sobrenome: string;
@@ -26,98 +25,123 @@ interface AppointmentsProps {
   data: string;
   hora: string;
   local: string;
-  idPaciente: string;
+  idPaciente: number;
   idMedico: string;
 }
 
-export default async function Patients() {
+export default function Patients() {
   const searchParams = useSearchParams();
-
   const search = searchParams.get("doctor");
-
   const doctorId = search;
 
-  console.log(doctorId);
+  const [filter, setFilter] = useState("today");
+  const [filteredAppointments, setFilteredAppointments] = useState<
+    AppointmentsProps[]
+  >([]);
+  const [patients, setPatients] = useState<PatientsProps[]>([]);
 
-  const patients: PatientesProps[] = await getPatients();
-  const appointments: AppointmentsProps[] = await getMedicalAppointments();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const patientsData: PatientsProps[] = await getPatients();
+        const appointments: AppointmentsProps[] =
+          await getMedicalAppointments();
 
-  const doctorAppointments = appointments.filter(
-    (appointment) => appointment.idMedico === doctorId
-  );
+        const doctorAppointments = appointments.filter(
+          (appointment) => appointment.idMedico === doctorId
+        );
 
-  const patientId = doctorAppointments.map((patient) => patient.idPaciente);
+        const today = dayjs().format("YYYY-MM-DD");
 
-  const pattientAppointments = patients.filter((patient) =>
-    patientId.includes(patient.id)
-  );
+        let filtered: AppointmentsProps[] = [];
+
+        if (filter === "today") {
+          filtered = doctorAppointments.filter(
+            (appointment) => appointment.data === today
+          );
+        } else if (filter === "past") {
+          filtered = doctorAppointments.filter(
+            (appointment) => appointment.data < today
+          );
+        } else if (filter === "future") {
+          filtered = doctorAppointments.filter(
+            (appointment) => appointment.data > today
+          );
+        }
+
+        setFilteredAppointments(filtered);
+        setPatients(patientsData);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
+  }, [doctorId, filter]);
+
+  const getPatientName = (idPaciente: number) => {
+    const patient = patients.find((p) => p.id === idPaciente);
+    return patient
+      ? `${patient.nome} ${patient.sobrenome}`
+      : "Paciente não encontrado";
+  };
 
   return (
     <div className="flex-grow p-10">
       <div className="rounded bg-gray-50 p-7">
-        {/* Select para filtrar pacientes */}
         <div className="mb-4 font-bold">
-          <select className="rounded-lg bg-gray-50 p-2">
-            <option value="">Hoje</option>
-            <option value="1">Ontem</option>
-            <option value="2">Antes de ontem</option>
+          <select
+            className="rounded-lg bg-gray-50 p-2"
+            onChange={(e) => setFilter(e.target.value)}
+            value={filter}
+          >
+            <option value="today">Hoje</option>
+            <option value="past">Passadas</option>
+            <option value="future">Futuras</option>
           </select>
         </div>
 
-        {/* Listagem de pacientes */}
         <div className="mt-7">
-          {pattientAppointments.map((patient) => (
-            <div key={patient.id} className="mb-4 ">
-              <div className="h-150 w-200 flex items-center  border-t-2 border-gray-300 py-2">
-                <div className="h-20 w-20 overflow-hidden rounded-full">
-                  {/* Sem imagem por enquanto */}
-                  <Image
-                    src={Pacientes}
-                    alt={patient.nome}
-                    width={100}
-                    height={100}
-                    className="h-full w-full object-cover"
-                  />
-                </div>
-                <div className="ml-4 grid w-full grid-cols-3 gap-4">
-                  <div className="max-w-48 overflow-hidden">
-                    <span className="block">
-                      {patient.nome} {patient.sobrenome}
-                    </span>
+          {filteredAppointments.length === 0 ? (
+            <p className="text-center">Não há consultas agendadas.</p>
+          ) : (
+            filteredAppointments.map((appointment) => (
+              <div key={appointment.id} className="mb-4">
+                <div className="h-150 w-200 flex items-center border-t-2 border-gray-300 py-2">
+                  <div className="h-20 w-20 overflow-hidden rounded-full">
+                    <Image
+                      src={Pacientes}
+                      alt={appointment.idPaciente.toString()}
+                      width={100}
+                      height={100}
+                      className="h-full w-full object-cover"
+                    />
                   </div>
-
-                  <div>
-                    {/* Exibir informações das consultas */}
-                    {doctorAppointments.map(
-                      (appointment, index) =>
-                        index === 0 && (
-                          <div key={appointment.id} className="flex space-x-2">
-                            <MdDateRange size={20} />
-                            <span>
-                              {dayjs(appointment.data).format(
-                                "D[ ]MMM[  ]YYYY"
-                              )}
-                            </span>
-                          </div>
-                        )
-                    )}
-                  </div>
-                  <div>
-                    {/* Exibir informações das consultas */}
-                    {doctorAppointments.map(
-                      (appointment, index) =>
-                        index === 0 && (
-                          <div key={appointment.id} className="flex space-x-2">
-                            <FiClock size={20} />
-                            <span>{appointment.hora}</span>
-                          </div>
-                        )
-                    )}
+                  <div className="ml-4 grid w-full grid-cols-3 gap-4">
+                    <div className="max-w-48 overflow-hidden">
+                      <span className="block">
+                        {getPatientName(appointment.idPaciente)}
+                      </span>
+                    </div>
+                    <div>
+                      <div className="flex space-x-2">
+                        <MdDateRange size={20} />
+                        <span>
+                          {dayjs(appointment.data).format("D[ ]MMM[ ]YYYY")}
+                        </span>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex space-x-2">
+                        <FiClock size={20} />
+                        <span>{appointment.hora}</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </div>
--- web/src/app/doctor/[doctorId]/patients/page.tsx
+++ web/src/app/doctor/[doctorId]/patients/page.tsx
@@ -1,29 +1,28 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-
-import { getPatients } from "../../../../services/get-patients";
 import Link from "next/link";
-
-import Pacientes from "../../../../assets/doctor1.png";
 import { MdDateRange } from "react-icons/md";
 import { FiClock } from "react-icons/fi";
-import { getMedicalAppointments } from "@/services/get-medical-appointments";
+import { getPatients } from "../../../../services/get-patients";
+import { getMedicalAppointments } from "../../../../services/get-medical-appointments";
 import { usePathname } from "next/navigation";
-
 import ptBr from "dayjs/locale/pt-br";
 import dayjs from "dayjs";
 
+import Patient from "../../../../assets/doctor1.png";
+
 dayjs.locale(ptBr);
 
-interface PatientesProps {
-  id: number;
+interface PatientsProps {
+  id: string;
   nome: string;
   sobrenome: string;
 }
 
 interface AppointmentsProps {
-  id: number;
+  id: string;
   data: string;
   hora: string;
   local: string;
@@ -31,95 +30,115 @@ interface AppointmentsProps {
   idMedico: string;
 }
 
-export default async function Patients() {
+export default function Patients() {
+  const [filter, setFilter] = useState("hoje");
+  const [patients, setPatients] = useState<PatientsProps[]>([]);
+  const [appointments, setAppointments] = useState<AppointmentsProps[]>([]);
+
   const pathname = usePathname();
-
-  // Divide o pathname em partes, separando-as pelos caracteres '/'
   const pathParts = pathname.split("/");
-
   const doctorId = pathParts[2];
 
-  const patients: PatientesProps[] = await getPatients();
-  const appointments: AppointmentsProps[] = await getMedicalAppointments();
+  useEffect(() => {
+    async function fetchData() {
+      const patientsData: PatientsProps[] = await getPatients();
+      const appointmentsData: AppointmentsProps[] =
+        await getMedicalAppointments();
+
+      setPatients(patientsData);
+      setAppointments(appointmentsData);
+    }
+
+    fetchData();
+  }, []);
 
   const doctorAppointments = appointments.filter(
     (appointment) => appointment.idMedico === doctorId
   );
 
-  const patientId = doctorAppointments.map((patient) => patient.idPaciente);
-
-  const pattientAppointments = patients.filter((patient) =>
-    patientId.includes(patient.id)
+  const patientIds = doctorAppointments.map(
+    (appointment) => appointment.idPaciente
   );
+
+  const patientAppointments = patients.filter((patient) =>
+    patientIds.includes(patient.id)
+  );
+
+  const filteredAppointments = () => {
+    switch (filter) {
+      case "hoje":
+        return doctorAppointments.filter((appointment) =>
+          dayjs(appointment.data).isSame(dayjs(), "day")
+        );
+      case "passadas":
+        return doctorAppointments.filter((appointment) =>
+          dayjs(appointment.data).isBefore(dayjs(), "day")
+        );
+      default:
+        return doctorAppointments;
+    }
+  };
 
   return (
     <div className="flex-grow p-10">
       <div className="rounded bg-gray-50 p-7">
-        {/* Select para filtrar pacientes */}
         <div className="mb-4 font-bold">
-          <select className="rounded-lg bg-gray-50 p-2">
-            <option value="">Hoje</option>
-            <option value="1">Consultas Passadas</option>
+          <select
+            className="rounded-lg bg-gray-50 p-2"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="hoje">Hoje</option>
+            <option value="passadas">Consultas Passadas</option>
+            {/* Adicione opções adicionais para futuras consultas, se necessário */}
           </select>
         </div>
 
-        {/* Listagem de pacientes */}
         <div className="mt-7">
-          {pattientAppointments.map((patient) => (
-            <div key={patient.id} className="mb-4 ">
-              <Link
-                href={`./patients/${patient.id}`}
-                className="h-150 w-200 flex items-center  border-t-2 border-gray-300 py-2"
-              >
-                <div className="h-20 w-20 overflow-hidden rounded-full">
-                  {/* Sem imagem por enquanto */}
-                  <Image
-                    src={Pacientes}
-                    alt={patient.nome}
-                    width={100}
-                    height={100}
-                    className="h-full w-full object-cover"
-                  />
-                </div>
-                <div className="ml-4 grid w-full grid-cols-3 gap-4">
-                  <div className="max-w-48 overflow-hidden">
-                    <span className="block">
-                      {patient.nome} {patient.sobrenome}
-                    </span>
+          {filteredAppointments().map((appointment) => {
+            const patient = patientAppointments.find(
+              (p) => p.id === appointment.idPaciente
+            );
+            return (
+              <div key={appointment.id} className="mb-4">
+                <Link
+                  href={`./patients/${patient?.id}`}
+                  className="h-150 w-200 flex items-center border-t-2 border-gray-300 py-2"
+                >
+                  <div className="h-20 w-20 overflow-hidden rounded-full">
+                    <Image
+                      src={Patient}
+                      alt={patient.nome}
+                      width={100}
+                      height={100}
+                      className="h-full w-full object-cover"
+                    />
                   </div>
-
-                  <div>
-                    {/* Exibir informações das consultas */}
-                    {doctorAppointments.map(
-                      (appointment, index) =>
-                        index === 0 && (
-                          <div key={appointment.id} className="flex space-x-2">
-                            <MdDateRange size={20} />
-                            <span>
-                              {dayjs(appointment.data).format(
-                                "D[ ]MMM[  ]YYYY"
-                              )}
-                            </span>
-                          </div>
-                        )
-                    )}
+                  <div className="ml-4 grid w-full grid-cols-3 gap-4">
+                    <div className="max-w-48 overflow-hidden">
+                      <span className="block">
+                        {patient?.nome} {patient?.sobrenome}
+                      </span>
+                    </div>
+                    <div>
+                      <div className="flex space-x-2">
+                        <MdDateRange size={20} />
+                        <span>
+                          {dayjs(appointment.data).format("D[ ]MMM[  ]YYYY")}
+                        </span>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex space-x-2">
+                        <FiClock size={20} />
+                        <span>{appointment.hora}</span>
+                      </div>
+                    </div>
                   </div>
-                  <div>
-                    {/* Exibir informações das consultas */}
-                    {doctorAppointments.map(
-                      (appointment, index) =>
-                        index === 0 && (
-                          <div key={appointment.id} className="flex space-x-2">
-                            <FiClock size={20} />
-                            <span>{appointment.hora}</span>
-                          </div>
-                        )
-                    )}
-                  </div>
-                </div>
-              </Link>
-            </div>
-          ))}
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
